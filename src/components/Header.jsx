b576f1b78e141142eb0b585584d7c52a1ec6ccb5
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-07-14 18:40:04 -0700

    Add an image to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import img from '../assets/img/tap-room.jpg';
 
 function Header(){
   return(
     <div>
     <style jsx> {`
       .header {
-        background-color: orange;
-        margin-bottom: 2%;
+        height:100px;
+        margin-bottom: 33%;
       }
       h1 {
         text-align: center;
+      }
+
+      #imgHeader {
+      width:80em;
+      height: 30em;
+      margin-left:10%;
+      margin-right:10%;
+
+
       }
 
     `}
@@ -22,8 +32,10 @@ function Header(){
     `}
     </style>
       <div className="header">
+        <Link to="/">Home</Link> <br></br><br></br> <Link to="/patron">Patron</Link> | <Link to="/employee">Employee</Link>
+        <img id="imgHeader" src={img}></img>
         <h1>SK Kombucha Bar</h1>
-          <Link to="/">Home</Link> | <Link to="/patron">Patron</Link> | <Link to="/employee">Employee</Link>
+        <hr/>
       </div>
     </div>
 
--- src/components/Patron.jsx
+++ src/components/Patron.jsx
@@ -6,7 +6,7 @@ function Patron(){
 
     <div>
       <p>Patron Component</p>
-      <Link to="/kegslist">Kegs List</Link>
+      <Link to="/kegslist">Kegs List</Link><br></br>
         <Link to="/pints">Pints</Link>
     </div>
   );
