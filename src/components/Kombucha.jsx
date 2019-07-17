1bc6377e93966e29b46180f7fd47b3ec61ad1633
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-07-17 14:30:21 -0700

    Add a tree component

--- src/components/Kombucha.jsx
+++ src/components/Kombucha.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 
 function Kombucha(props){
-      console.log(props.kombuchaId)
   const kombuchaInformation =
   <div>
     <h3>{props.brandKombucha}</h3>
